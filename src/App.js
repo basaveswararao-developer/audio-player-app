db4c0be2f35f8694d05b6b2239f44435ea95475e
basaveswararao <nbasaveswararao07@gmail.com>
2024-02-19 11:32:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [playlist, setPlaylist] = useState([]);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [audioElement, setAudioElement] = useState(null);
+
+  useEffect(() => {
+    const lastPlayedTrackIndex = localStorage.getItem('lastPlayedTrackIndex');
+    if (lastPlayedTrackIndex) {
+      setCurrentTrackIndex(parseInt(lastPlayedTrackIndex));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('lastPlayedTrackIndex', currentTrackIndex.toString());
+  }, [currentTrackIndex]);
+
+  const handleFileUpload = (event) => {
+    const files = event.target.files;
+    const updatedPlaylist = [];
+    for (let i = 0; i < files.length; i++) {
+      updatedPlaylist.push(URL.createObjectURL(files[i]));
+    }
+    setPlaylist(updatedPlaylist);
+  };
+
+  const handlePlay = (index) => {
+    setCurrentTrackIndex(index);
+    if (audioElement) {
+      audioElement.play();
+    }
+  };
+
+  const handlePause = () => {
+    if (audioElement) {
+      audioElement.pause();
+    }
+  };
+
+  const handleEnded = () => {
+    setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % playlist.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='centered-container'>
+      <input type="file" multiple onChange={handleFileUpload} />
+      <div className='audio'>
+        <audio src={playlist[currentTrackIndex]} controls ref={(element) => setAudioElement(element)} onEnded={handleEnded}/>
+        <div className='btn'>
+          <button onClick={handlePlay.bind(null, currentTrackIndex)}>Play</button>
+          <button onClick={handlePause}>Pause</button>
+        </div>
+      </div>
+      <div>
+        {playlist.map((track, index) => (
+          <div key={index}>
+            <button onClick={() => handlePlay(index)}>Play</button>
+            <span>{`Track ${index + 1}`}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
